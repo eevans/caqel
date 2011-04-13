335dace09eeed630897d5cf418cde001b77096c2
Eric Evans <eevans@sym-link.com>
2011-04-13 12:13:09 -0500

    updated for driver conversion to dbapi2

--- query/views.py
+++ query/views.py
@@ -6,42 +6,45 @@ from django.conf import settings
 
 from json import dumps as to_json
 from uuid import UUID
-from cql.connection import Connection
-from cql.results import RowsProxy
-from cql.errors import CQLException
+
+import cql
+from cql.results import ResultSet
  
 import threading
 
 __local = threading.local()
 
-def __get_connection():
-    return Connection(settings.CASSANDRA_HOST, settings.CASSANDRA_PORT)
+def __get_cursor():
+    return cql.connect(settings.CASSANDRA_HOST,
+                       settings.CASSANDRA_PORT).cursor()
 
 def __execute(query, keyspace=None):
-    # Lazily assign a connection instance to a thread-local variable
-    if not hasattr(__local, "conn") or not getattr(__local, "conn"):
-        __local.conn = __get_connection()
+    # Lazily assign a cursor instance to a thread-local variable
+    if not hasattr(__local, "cursor") or not getattr(__local, "cursor"):
+        __local.cursor = __get_cursor()
     if keyspace:
-        __local.conn.execute("USE " + keyspace)
-    return __local.conn.execute(query)
+        __local.cursor.execute("USE " + keyspace)
+        
+    __local.cursor.execute(query)
+    return __local.cursor
 
-def __serialize(results):
+def __serialize(cursor):
     def marshal(value):
         if isinstance(value, UUID):
             return str(value)
         return value
         
-    if isinstance(results, RowsProxy):
+    if isinstance(cursor.result, ResultSet):
         rows = {}
-        for result in results:
-            rows[result.key] = []
-            for col in result:
-                rows[result.key].append(
+        for row in cursor.result.rows:
+            rows[row.key] = []
+            for col in row.columns:
+                rows[row.key].append(
                     {"name": marshal(col.name), "value": marshal(col.value)})
         return to_json({"rows": rows})
     else:
-        if results is None: return to_json({"void": "Success"})
-        else: return to_json({"int": results})
+        if cursor.result is None: return to_json({"void": "Success"})
+        else: return to_json({"int": cursor.result})
 
 # View methods
 def index(request):
@@ -57,14 +60,14 @@ def query(request):
             json = to_json({"void": "Using keyspace %s" % keyspace})
         else:
             current_keyspace = request.session.get("current_keyspace", None)
-            results = __execute(query_string, current_keyspace)
-            json = __serialize(results)
-    except CQLException, error:
+            cursor = __execute(query_string, current_keyspace)
+            json = __serialize(cursor)
+    except cql.DatabaseError, error:
         json = to_json({"exception": str(error)})
     return HttpResponse(json, mimetype="application/json")
     
 def describe_keyspaces(request):
-    schema = __get_connection().decoder.schema    # you gotta keep 'em violated
+    schema = __get_cursor().decoder.schema    # you gotta keep 'em violated
     del schema["system"]
     return HttpResponse(to_json(schema), mimetype="application/json")
 
